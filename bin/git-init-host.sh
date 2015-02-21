dcfa5c623abae9a0db5b99b0b3664e269fb10256
Smotrov Dmitriy <dsxack@gmail.com>
2015-02-21 22:52:08 +0300

    Добавлена установка удаленного источника bitbucket

--- bin/git-init-host.sh
+++ bin/git-init-host.sh
@@ -9,12 +9,18 @@ fi
 
 cd "${HOME}/www/${REPO}.app-web.ru"
 git init
+
 git config core.filemode false
-git remote add origin "${HOME}/repo/${REPO}.app-web.ru.git"
-git add .
 git config --global user.email "${REPO}@app-web.ru"
 git config --global user.name "${REPO} on app-web.ru"
+
+git remote add origin "${HOME}/repo/${REPO}.app-web.ru.git"
+git remote add bitbucket "git@bitbucket.org:worksolutions-company/${REPO}.git"
+
+git add .
 git commit -m "init commit"
 git push -u origin master
+
 cat "${DIR}/../dist/post-update" | sed "s/#HOSTNAME#/${REPO}/"  > "${HOME}/repo/${REPO}.app-web.ru.git/hooks/post-update"
 chmod +x "${HOME}/repo/${REPO}.app-web.ru.git/hooks/post-update"
+
