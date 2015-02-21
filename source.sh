27cda15cd02906cefb058b63842567e85d6e3fb5
Smotrov Dmitriy <dsxack@gmail.com>
2015-02-21 23:04:45 +0300

    Исправление рекурсии

--- source.sh
+++ source.sh
@@ -3,7 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PATH="${PATH}:${DIR}/bin"
 
-if [ ${SHELL} -ne "/usr/bin/zsh" ];
+if [ "${SHELL}" -ne "/usr/bin/zsh" ];
 then
     zsh && exit
 fi
